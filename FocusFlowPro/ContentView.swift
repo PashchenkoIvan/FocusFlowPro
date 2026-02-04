157141427931afcbb4cded9bfb6830c170559ac0
IvanPashchenko <pashchenkoivanforwork@gmail.com>
2026-02-04 11:15:19 +0100

    Initial Commit

---
+++ FocusFlowPro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FocusFlowPro
+//
+//  Created by Пащенко Иван on 04.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FocusFlowPro/FocusFlowProApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusFlowProApp.swift
+//  FocusFlowPro
+//
+//  Created by Пащенко Иван on 04.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct FocusFlowProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
